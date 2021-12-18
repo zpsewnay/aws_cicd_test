286e1305fbef60558f7ba0f21df94e519b9e345d
C.C.Sun <32284495+zpsewnay@users.noreply.github.com>
2021-12-18 18:57:10 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,8 +5,8 @@ USERNAME=106260173616.dkr.ecr.ap-northeast-1.amazonaws.com
 IMAGE=aws_final_project
 docker build -t $USERNAME/$IMAGE:latest .
 echo "image builded !"
-version=`cat VERSION`
-echo "version: $version"
-docker tag $USERNAME/$IMAGE:latest $USERNAME/$IMAGE:$version
-echo "image TAG done !"
+#version=`cat VERSION`
+#echo "version: $version"
+#docker tag $USERNAME/$IMAGE:latest $USERNAME/$IMAGE:$version
+#echo "image TAG done !"
 docker push $USERNAME/$IMAGE:$version
