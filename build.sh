db9b10787f57708ca17edcd4366686522d8f313d
C.C.Sun <32284495+zpsewnay@users.noreply.github.com>
2021-12-18 15:50:28 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,9 +2,11 @@ set -ex
 #set registry
 USERNAME=106260173616.dkr.ecr.ap-northeast-1.amazonaws.com
 # image name
-IMAGE=cfc103_student03
+IMAGE=aws_final_project
 docker build -t $USERNAME/$IMAGE:latest .
+echo "image builded !"
 version=`cat VERSION`
 echo "version: $version"
 docker tag $USERNAME/$IMAGE:latest $USERNAME/$IMAGE:$version
+echo "image TAG done !"
 docker push $USERNAME/$IMAGE:$version
